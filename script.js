0f2f941828cdba3f30568304ee562722f30db306
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-10-28 00:39:47 +0600

    some refacroting

--- script.js
+++ script.js
@@ -7,19 +7,17 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 			return;  
 		}
 		res.json().then(data => {  
-			data.forEach((item, i, arr) => {
-				let div = document.createElement('div');div.className = 'post';
-				let h3 = document.createElement('h3'); 	h3.className = 'title';
-				let p = document.createElement('p'); 	p.className = 'descr';
-				section.appendChild(div)
-				div.appendChild(h3); div.appendChild(p);
-				h3.innerHTML = item.title;
-				p.innerHTML = item.body; 
+			data.forEach((item, i, arr) => { 
+				let content = ` 
+					<h3 class='title'>${item.title}</h3>
+					<p class='descr'>${item.body}</p> 
+				` 
+				let wrapper = document.createElement('div'); 
+				wrapper.className = 'post'
+				wrapper.innerHTML = content 
+				section.appendChild(wrapper)
 			}) 
 		});
-	})
-	.catch((err) => {
+	}).catch((err) => {
 		console.error(err);
-	});
-
- 
+	}); 
